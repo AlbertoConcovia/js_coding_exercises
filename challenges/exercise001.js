b0c9c73f6d160ba08f3ac80a7423db43ff584b29
albertoconcovia <concovia@gmail.com>
2021-09-27 14:00:07 +0100

    Implemented exercise001 - reverseWord

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -41,6 +41,10 @@ function getMiddleCharacter(str) {
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  //  expect(reverseWord("foo")).toBe("oof");
+  //  expect(reverseWord("why would you even want to do this?")).toBe("?siht od ot tnaw neve uoy dluow yhw"}
+  console.log(word.split("").reverse().join(""));
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
