d9fbbe01207390a54d7c08754b79aaaeb29eee86
albertoconcovia <concovia@gmail.com>
2021-09-29 12:27:44 +0100

    Implemented exercise004 - map & filter

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,9 +1,13 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // for (let i = 0; i < nums.length; i++) {
-  const newArray = [];
-  nums.forEach(function (item) {
-    if (item < 1) newArray.push(item);
+  // test "returns an array of numbers smaller than 1"
+  // Filter function
+  const newArray = nums.filter(function (item) {
+    if (item < 1) {
+      return true;
+    } else {
+      return false;
+    }
   });
   return newArray;
 }
@@ -11,60 +15,69 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // const newArrayNames = []
-  // for (let i = 0; i < names.length; i++) {
-  //   if ( names[i].substring(0,1) === char ) {
-  //     newArrayNames.push(names[i]);
-  //   }
-  // }
-  // return newArrayNames;
-  const newArrayNames = [];
-  names.forEach(function (item) {
-    if (item.substring(0, 1) === char) newArrayNames.push(item);
+  // test "returns an array of names beginning with the specified character"
+  // filter function
+  const newArrayNames = names.filter(function (item) {
+    if (item.substring(0, 1) === char) {
+      return true;
+    } else {
+      return false;
+    }
   });
   return newArrayNames;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  const newArrayWords = [];
-  // for (let i = 0; i < words.length; i++) {
-  words.forEach(function (item) {
-    if (item.substring(0, 3) === "to ") newArrayWords.push(item);
+  //test("returns an array of words that are considered verbs (because they begin with 'to ')"
+  // filter function
+  // return the new array
+  const newArrayWords = words.filter(function (item) {
+    if (item.substring(0, 3) === "to ") {
+      return true;
+    } else {
+      return false;
+    }
   });
   return newArrayWords;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  const arrayIngeter = [];
-  // for (let i = 0; i < nums.length; i++) {
-  nums.forEach(function (item) {
-    if (Number.isInteger(item) === true) arrayIngeter.push(item);
+  // test "returns an array containing only integers"
+  // filter function
+  // retun new arrayInteger
+  const arrayIngeter = nums.filter(function (item) {
+    if (Number.isInteger(item) === true) {
+      return true;
+    } else {
+      return false;
+    }
   });
   return arrayIngeter;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  const newArrayUsers = [];
-  // for ( let i=0 ; i < users.length; i++ ) {
-  users.forEach(function (item) {
-    newArrayUsers.push(item.data.city.displayName);
+  // test "returns an array of the cities of each user"
+  // map function
+  // return new array newArrayUsers
+  const newArrayUsers = users.map(function (item) {
+    return item.data.city.displayName;
   });
   return newArrayUsers;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  const arraySquareRoots = [];
-  // for ( let i=0 ; i < nums.length; i++ ) {
-  nums.forEach((item) => {
+  //test "gets the square root of each number to 2 decimal places"
+  // map function
+  const arraySquareRoots = nums.map((item) => {
     let squareRoot = Math.sqrt(item);
     if (Number.isInteger(squareRoot) === false) {
       squareRoot = squareRoot.toFixed(2);
     }
-    arraySquareRoots.push(Number(squareRoot));
+    return Number(squareRoot);
   });
   return arraySquareRoots;
 }
@@ -72,23 +85,25 @@ function getSquareRoots(nums) {
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  const arraySentence = [];
-  // for ( let i=0 ; i < sentences.length; i++ ) {
-  sentences.forEach((e) => {
-    if (e.toUpperCase().search(str.toUpperCase()) >= 0) arraySentence.push(e);
+  //test"returns only the sentences containing the specified string"
+  //filter function
+  const arraySentence = sentences.filter((e) => {
+    if (e.toUpperCase().search(str.toUpperCase()) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
   });
   return arraySentence;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  var arrayLongestSide = [];
-  triangles.forEach(function (item) {
-    var longestSide = 0;
-    item.forEach(function (num) {
-      if (num > longestSide) longestSide = num;
-    });
-    arrayLongestSide.push(longestSide);
+  // test "returns the longest side of each set of triangle data"
+  // map function
+  // Math.max function
+  var arrayLongestSide = triangles.map(function (item) {
+    return Math.max(...item);
   });
   return arrayLongestSide;
 }
