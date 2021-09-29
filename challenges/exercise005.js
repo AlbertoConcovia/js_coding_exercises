652d612adadcffed623218296620829fdc9219df
albertoconcovia <concovia@gmail.com>
2021-09-29 20:39:44 +0100

    Implemented exercise005 - findNextNumber

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,25 +1,32 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  // test "returns the next number after the given number in the array"
+  // find the index of the number in the array
+  // increment index by one  and find the new number
+  // return the next nummber
+  var indexNum = nums.indexOf(n);
+  if (indexNum === -1 || indexNum+1 === nums.length ) return null;
+  const foundNextNum = nums.find((value, index) => index === indexNum + 1);
+  return foundNextNum;
 };
 
-const count1sand0s = str => {
+const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
 };
 
-const reverseNumber = n => {
+const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
   // Your code here!
 };
 
-const sumArrays = arrs => {
+const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
   // Your code here!
 };
 
-const arrShift = arr => {
+const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
 };
@@ -30,7 +37,7 @@ const findNeedle = (haystack, searchTerm) => {
   // Your code here!
 };
 
-const getWordFrequencies = str => {
+const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
 };
@@ -42,5 +49,5 @@ module.exports = {
   sumArrays,
   arrShift,
   findNeedle,
-  getWordFrequencies
+  getWordFrequencies,
 };
