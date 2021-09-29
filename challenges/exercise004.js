9d8bbda849589a705450dd69de00c4b1d43e80b9
albertoconcovia <concovia@gmail.com>
2021-09-29 09:58:14 +0100

    Implemented exercise004 - forEach

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,87 +1,96 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  const newArray = []
-  for (let i = 0; i < nums.length; i++) {
-    if (Number(nums[i]) < 1) newArray.push(nums[i]);
-  }
-  return newArray;  
+  // for (let i = 0; i < nums.length; i++) {
+  const newArray = [];
+  nums.forEach(function (item) {
+    if (item < 1) newArray.push(item);
+  });
+  return newArray;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  const newArrayNames = []
-  for (let i = 0; i < names.length; i++) {
-    if ( names[i].substring(0,1) === char ) {
-      newArrayNames.push(names[i]);
-    }
-  }
+  // const newArrayNames = []
+  // for (let i = 0; i < names.length; i++) {
+  //   if ( names[i].substring(0,1) === char ) {
+  //     newArrayNames.push(names[i]);
+  //   }
+  // }
+  // return newArrayNames;
+  const newArrayNames = [];
+  names.forEach(function (item) {
+    if (item.substring(0, 1) === char) newArrayNames.push(item);
+  });
   return newArrayNames;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  const newArrayWords = []
-  for (let i = 0; i < words.length; i++) {
-    if ( words[i].substring(0,3) === 'to ' ) newArrayWords.push(words[i]);
-  }
+  const newArrayWords = [];
+  // for (let i = 0; i < words.length; i++) {
+  words.forEach(function (item) {
+    if (item.substring(0, 3) === "to ") newArrayWords.push(item);
+  });
   return newArrayWords;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  const arrayIngeter = []
-  for (let i = 0; i < nums.length; i++) {
-    if ( Number.isInteger(nums[i]) === true ) arrayIngeter.push(nums[i]);
-  }
+  const arrayIngeter = [];
+  // for (let i = 0; i < nums.length; i++) {
+  nums.forEach(function (item) {
+    if (Number.isInteger(item) === true) arrayIngeter.push(item);
+  });
   return arrayIngeter;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  const newArrayUsers=[];
-  for ( let i=0 ; i < users.length; i++ ) {
-    newArrayUsers.push(users[i].data.city.displayName);
-  }
+  const newArrayUsers = [];
+  // for ( let i=0 ; i < users.length; i++ ) {
+  users.forEach(function (item) {
+    newArrayUsers.push(item.data.city.displayName);
+  });
   return newArrayUsers;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  const arraySquareRoots=[];
-  for ( let i=0 ; i < nums.length; i++ ) {
-    let squareRoot = Math.sqrt((nums[i]));
+  const arraySquareRoots = [];
+  // for ( let i=0 ; i < nums.length; i++ ) {
+  nums.forEach((item) => {
+    let squareRoot = Math.sqrt(item);
     if (Number.isInteger(squareRoot) === false) {
       squareRoot = squareRoot.toFixed(2);
     }
     arraySquareRoots.push(Number(squareRoot));
-  }
+  });
   return arraySquareRoots;
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  const arraySentence =[];
-  for ( let i=0 ; i < sentences.length; i++ ) {
-    if ( sentences[i].toUpperCase().search(str.toUpperCase()) >= 0 ){
-      arraySentence.push(sentences[i]);
-    }
-  }
+  const arraySentence = [];
+  // for ( let i=0 ; i < sentences.length; i++ ) {
+  sentences.forEach((e) => {
+    if (e.toUpperCase().search(str.toUpperCase()) >= 0) arraySentence.push(e);
+  });
   return arraySentence;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  var arrayLongestSide = [] ;
-  for (let i=0; i < triangles.length; i++) {
+  var arrayLongestSide = [];
+  triangles.forEach(function (item) {
     var longestSide = 0;
-    for (let x=0; x < triangles[x].length; x++) {
-      if (triangles[i][x] > longestSide) longestSide = triangles[i][x];
-    }
+    item.forEach(function (num) {
+      if (num > longestSide) longestSide = num;
+    });
     arrayLongestSide.push(longestSide);
-  }
-    return arrayLongestSide;
+  });
+  return arrayLongestSide;
 }
 
 module.exports = {
@@ -92,5 +101,5 @@ module.exports = {
   getCities,
   getSquareRoots,
   findSentencesContaining,
-  getLongestSides
+  getLongestSides,
 };
